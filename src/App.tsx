b69001197ceaf85dfce8be3990b2df7419696012
Libby Greenlaw <33658445+lgreenlaw2022@users.noreply.github.com>
2023-07-25 18:30:10 -0400

    TEST

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <img src={buildingGIF} alt="wait until the page loads" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit HELLOO!!! <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
