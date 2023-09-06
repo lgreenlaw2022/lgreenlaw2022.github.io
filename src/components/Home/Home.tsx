9e29b3b30374d561cc8edf8daccee31cf4a0283c
Libby Greenlaw <33658445+lgreenlaw2022@users.noreply.github.com>
2023-09-06 15:51:33 -0300

    home text and style updates

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,7 +5,9 @@ import './Home.css';
 function Home() {
   return (
     <div className="Home-container">
-      Welcome
+      <div style={{display: 'flex', justifyContent: 'flex-start'}}>Welcome</div>
+      <div style={{display: 'flex', justifyContent: 'flex-end', color: '#2f2f2f'}}>I'm Libby</div>
+      
     </div>
   );
 }
