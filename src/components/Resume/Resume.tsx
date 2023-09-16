5709ef30a5e150204040023b74c74dd11fab1524
Libby Greenlaw <33658445+lgreenlaw2022@users.noreply.github.com>
2023-09-15 21:58:03 -0300

    added resume and link

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Resume.css';
+import resume from "../../assets/resume-screenshot.jpg"
 
 
 function Resume() {
   return (
     <div className="resume-Container">
       <div className="resume-Text">View My Resume</div>
+      
+      <a
+        href="https://docs.google.com/document/d/e/2PACX-1vRblP1I5kzgBNp8K6adJhf2G58zYSsXrbQwqRjBKemtvpU4U6jSERLpDRBiBBspuKzWKLYI87Md8N-X/pub"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="Image" src={resume} alt="resume thumbnail" />
+      </a>
     </div>
   );
 }
